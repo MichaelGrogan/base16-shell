867e735af61e35e9e43d85a413d7cdd51f3acfcd
michael <muximori@gmail.com>
2021-06-18 14:23:46 +1000

    simplified config merge. no longer needs yq

--- profile_helper.sh
+++ profile_helper.sh
@@ -29,11 +29,10 @@ _base16()
   fi
 
 	if [ \
-		$(type yq >> /dev/null;echo $?) = 0 \
-		-a -f ~/dotfiles/alacritty.yml \
+		-f ~/dotfiles/alacritty.yml \
 		-a -d ~/.config/alacritty/base16-alacritty \
 		]; then
-			yq m -ix ~/dotfiles/alacritty.yml ~/.config/alacritty/base16-alacritty/colors/base16-"$BASE16_THEME"-256.yml
+			cp ~/.config/alacritty/base16-alacritty/colors/base16-"$BASE16_THEME"-256.yml ~/.config/alacritty/colorscheme.yml
 			[ ! $? = 0 ] && echo Error merging colorscheme to alacritty config. 
 		else
 			echo cannot apply colorscheme to alacritty. Missing files.
